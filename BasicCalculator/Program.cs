e72e7f38c3c21d86479dbf4b71b59dc1c9907eef
RAlanWright <31636206+RAlanWright@users.noreply.github.com>
2019-06-04 16:36:55 -0500

    Added some basic input and addition functionality

--- BasicCalculator/Program.cs
+++ BasicCalculator/Program.cs
@@ -10,6 +10,14 @@ namespace BasicCalculator
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("{0} + {1} = {2}", num1, num2, num1 + num2);
+            Console.ReadLine();
+
         }
     }
 }
